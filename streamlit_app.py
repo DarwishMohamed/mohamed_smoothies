4829a313ad7941209bc90e3b6234f8cb6d32b402
DarwishMohamed <109226826+DarwishMohamed@users.noreply.github.com>
2024-06-16 16:06:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,12 @@ if ingredients_list:
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
 
+# New section to display fruityvice nutrition information
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
 
+# Get the response from the Fruityvice API
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+# Display the data in a dataframe
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
