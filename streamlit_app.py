cc93952b7ddfb00e06b7b624fc9aa1764593ff1e
DarwishMohamed <109226826+DarwishMohamed@users.noreply.github.com>
2024-06-17 10:28:41 +0100

    Update streamlit_app.py
    Lesson 10 Using FruityVice Data more effectively

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
+import requests 
 # Write directly to the app
 st.title("Customize Your Smoothie:smoothie:")
 st.write("E5tar el fakha el enta 3ayezha w engez mat2refnash"
@@ -21,10 +21,13 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-    ingredients_string = ''
+        ingredients_string = ''
 
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
+        for fruit_chosen in ingredients_list:
+            ingredients_string += fruit_chosen + ' '
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+            fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True) 
+
 
     # st.write(ingredients_string)
 
@@ -41,7 +44,7 @@ if ingredients_list:
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
+
 fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True) 
