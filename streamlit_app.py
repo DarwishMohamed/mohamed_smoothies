dae4c4ac76c6196e369a6d224928d1833a559903
DarwishMohamed <109226826+DarwishMohamed@users.noreply.github.com>
2024-06-17 14:27:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests 
+import requests
+import hashlib
 
 # Write directly to the app
 st.title("Customize Your Smoothie 🍹")
@@ -16,35 +17,32 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
-
 # Convert the Snowpark dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 #st.stop()
 
-ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients',
-    my_dataframe
-)
+ingredients_list = st.multiselect('Choose up to 5 ingredients', my_dataframe)
 
 if ingredients_list:
-    ingredients_string = ''
+    ingredients_string = ', '.join(ingredients_list)
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-        
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
 
         st.subheader(fruit_chosen + 'Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True) 
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
-    # st.write(ingredients_string)
+    # Calculate the hash for the ingredients
+    hash_ing = calculate_hash(ingredients_list)
 
-    my_insert_stmt = """ 
-    INSERT INTO smoothies.public.orders(ingredients, name_on_order)
-    VALUES ('""" + ingredients_string.strip() + """', '""" + name_on_order + """')"""
+    # Create the INSERT statement including the hash_ing
+    my_insert_stmt = f"""
+    INSERT INTO smoothies.public.orders(ingredients, name_on_order, hash_ing)
+    VALUES ('{ingredients_string}', '{name_on_order}', {hash_ing})
+    """
 
     st.write(my_insert_stmt)
 
