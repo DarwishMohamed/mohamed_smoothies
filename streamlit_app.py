6f7526fc3dbd515395f6fe174a244876a640147a
DarwishMohamed <109226826+DarwishMohamed@users.noreply.github.com>
2024-06-17 12:42:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests
+import requests 
 import hashlib
 
 # Write directly to the app
@@ -16,48 +16,60 @@ order_filled = st.checkbox('Mark order as filled')
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
 
 # Convert the Snowpark dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients',
     my_dataframe
 )
 
-def calculate_hash(ingredients):
+def calculate_hash_using_fruit_name(ingredients):
     ingredients_string = ' '.join(ingredients).strip()
     return int(hashlib.md5(ingredients_string.encode()).hexdigest(), 16)
 
+def calculate_hash_using_search_on(ingredients):
+    ingredients_string = ' '.join([pd_df.loc[pd_df['FRUIT_NAME'] == fruit, 'SEARCH_ON'].iloc[0] for fruit in ingredients]).strip()
+    return int(hashlib.md5(ingredients_string.encode()).hexdigest(), 16)
+
+def calculate_hash_concatenated(ingredients):
+    concatenated_string = ' '.join(ingredients).strip() + ' ' + ' '.join([pd_df.loc[pd_df['FRUIT_NAME'] == fruit, 'SEARCH_ON'].iloc[0] for fruit in ingredients]).strip()
+    return int(hashlib.md5(concatenated_string.encode()).hexdigest(), 16)
+
 if ingredients_list:
     ingredients_string = ''
-    search_on_string = ''
-    concatenated_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        search_on_string += search_on + ' '
-        concatenated_string += fruit_chosen + ' ' + search_on + ' '
-
-        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         st.subheader(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True) 
 
-    hash_fruit_name = calculate_hash(ingredients_list)
-    hash_search_on = calculate_hash(search_on_string.strip().split())
-    hash_concatenated = calculate_hash(concatenated_string.strip().split())
+    # Calculate hashes using different methods
+    hash_ing_fruit_name = calculate_hash_using_fruit_name(ingredients_list)
+    hash_ing_search_on = calculate_hash_using_search_on(ingredients_list)
+    hash_ing_concatenated = calculate_hash_concatenated(ingredients_list)
 
-    st.write(f"Hash using FRUIT_NAME: {hash_fruit_name}")
-    st.write(f"Hash using SEARCH_ON: {hash_search_on}")
-    st.write(f"Hash using concatenated string: {hash_concatenated}")
+    st.write(f"Hash using FRUIT_NAME: {hash_ing_fruit_name}")
+    st.write(f"Hash using SEARCH_ON: {hash_ing_search_on}")
+    st.write(f"Hash using concatenated string: {hash_ing_concatenated}")
 
-    # Use the hash based on FRUIT_NAME for the orders table
-    my_insert_stmt = f"""
+    # Choose the hash method to use
+    hash_ing = hash_ing_fruit_name  # You can change this to hash_ing_search_on or hash_ing_concatenated for testing
+
+    # Construct the SQL insert statement
+    my_insert_stmt = """
     INSERT INTO smoothies.public.orders(ingredients, name_on_order, order_filled, hash_ing)
-    VALUES ('{ingredients_string.strip()}', '{name_on_order}', '{str(order_filled).upper()}', {hash_fruit_name})
+    VALUES ('""" + ingredients_string.strip() + """', '""" + name_on_order + """', '""" + str(order_filled).upper() + """', """ + str(hash_ing) + """)
     """
 
     st.write(my_insert_stmt)
