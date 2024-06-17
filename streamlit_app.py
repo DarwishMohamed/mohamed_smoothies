0173f9bfee7281060ced13a485a3f22d20065f27
DarwishMohamed <109226826+DarwishMohamed@users.noreply.github.com>
2024-06-17 10:10:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 # Import python packages
 import streamlit as st
+from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-import requests
+
 # Write directly to the app
 st.title("Customize Your Smoothie:smoothie:")
 st.write("E5tar el fakha el enta 3ayezha w engez mat2refnash"
@@ -10,11 +11,9 @@ st.write("E5tar el fakha el enta 3ayezha w engez mat2refnash"
 # Input for name on order
 name_on_order = st.text_input('Name on Order', '')
 
-cnx = st.connection("snowflake")
-session = cnx.session()
+session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients'
@@ -26,10 +25,6 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + 'Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-
 
     # st.write(ingredients_string)
 
