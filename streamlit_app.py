fba10da0b55d7a161314c19d823dce0ff3445d51
DarwishMohamed <109226826+DarwishMohamed@users.noreply.github.com>
2024-06-17 15:09:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,7 @@
-
-
-
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests 
+import requests
 
 # Write directly to the app
 st.title("Customize Your Smoothie 🍹")
@@ -13,12 +10,14 @@ st.write("E5tar el fakha el enta 3ayezha w engez mat2refnash")
 # Input for name on order
 name_on_order = st.text_input('Name on Order', '')
 
+# Select box for order filled status
+order_filled = st.selectbox('Is the order filled?', ['No', 'Yes'])
+
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
-
 
 # Convert the Snowpark dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
@@ -43,11 +42,15 @@ if ingredients_list:
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True) 
 
+    # Convert order filled status to boolean
+    order_filled_bool = 'TRUE' if order_filled == 'Yes' else 'FALSE'
+    
     # st.write(ingredients_string)
 
-    my_insert_stmt = """ 
-    INSERT INTO smoothies.public.orders(ingredients, name_on_order)
-    VALUES ('""" + ingredients_string.strip() + """', '""" + name_on_order + """')"""
+    my_insert_stmt = f""" 
+    INSERT INTO smoothies.public.orders(ingredients, name_on_order, order_filled)
+    VALUES ('{ingredients_string.strip()}', '{name_on_order}', {order_filled_bool})
+    """
 
     st.write(my_insert_stmt)
 
