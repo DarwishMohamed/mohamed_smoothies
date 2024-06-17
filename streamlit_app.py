f6a46cea98316d24318ebcd4b0dfbd55eebc1d4a
DarwishMohamed <109226826+DarwishMohamed@users.noreply.github.com>
2024-06-17 13:14:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,19 +34,18 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
 
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     hash_ing = calculate_hash(ingredients_list)
-
     st.write(f"Calculated hash: {hash_ing}")
 
-    my_insert_stmt = """
+    my_insert_stmt = f"""
     INSERT INTO smoothies.public.orders(ingredients, name_on_order, order_filled, hash_ing)
-    VALUES ('""" + ingredients_string + """', '""" + name_on_order + """', '""" + str(order_filled).upper() + """', '""" + str(hash_ing) + """')
+    VALUES ('{ingredients_string}', '{name_on_order}', '{str(order_filled).upper()}', '{str(hash_ing)}')
     """
 
     st.write(my_insert_stmt)
