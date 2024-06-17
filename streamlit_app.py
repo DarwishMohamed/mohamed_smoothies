6b3429303e5acf9f6b8cb5b8f492a09a35e404ce
DarwishMohamed <109226826+DarwishMohamed@users.noreply.github.com>
2024-06-17 12:10:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
-import hashlib
+# Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests
+import requests 
+import hashlib
 
 # Write directly to the app
 st.title("Customize Your Smoothie 🍹")
@@ -24,7 +25,9 @@ ingredients_list = st.multiselect(
     my_dataframe
 )
 
-def calculate_hash(ingredients_string):
+def calculate_hash(ingredients):
+    ingredients_string = ' '.join(ingredients).strip()
+    # Calculate MD5 hash and convert to an integer
     return int(hashlib.md5(ingredients_string.encode()).hexdigest(), 16)
 
 if ingredients_list:
@@ -35,10 +38,11 @@ if ingredients_list:
         st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
 
         st.subheader(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
-    hash_ing = calculate_hash(ingredients_string)
+    # Now calculate the hash using the ingredients_string
+    hash_ing = calculate_hash(ingredients_list)
 
     my_insert_stmt = """
     INSERT INTO smoothies.public.orders(ingredients, name_on_order, order_filled, hash_ing)
