8cd6162f0d1d654be3ffe3c6efd6460efcf7d2fb
DarwishMohamed <109226826+DarwishMohamed@users.noreply.github.com>
2024-06-17 13:22:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,50 +30,51 @@ def calculate_hash(ingredients, delimiter):
     return int(hashlib.md5(ingredients_string.encode()).hexdigest(), 16)
 
 if ingredients_list:
-    ingredients_string = ', '.join(ingredients_list)
-    
-    for fruit_chosen in ingredients_list:
-        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        fruit_option = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'FRUIT_NAME'].iloc[0]
-        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
-        st.write('The fruit option value for ', fruit_chosen, ' is ', fruit_option, '.')
-
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-
-    # Calculate hashes using different delimiters and sources
-    hash_using_fruit_name_space = calculate_hash(ingredients_list, ' ')
-    hash_using_fruit_name_comma = calculate_hash(ingredients_list, ',')
-    hash_using_fruit_name_comma_space = calculate_hash(ingredients_list, ', ')
+    # Generate ingredient strings using different sources
+    ingredients_string_comma_space = ', '.join(ingredients_list)
+    ingredients_string_space = ' '.join(ingredients_list)
+    ingredients_string_comma = ','.join(ingredients_list)
 
     search_on_list = [pd_df.loc[pd_df['FRUIT_NAME'] == fruit, 'SEARCH_ON'].iloc[0] for fruit in ingredients_list]
-    hash_using_search_on_space = calculate_hash(search_on_list, ' ')
-    hash_using_search_on_comma = calculate_hash(search_on_list, ',')
-    hash_using_search_on_comma_space = calculate_hash(search_on_list, ', ')
+    search_on_string_comma_space = ', '.join(search_on_list)
+    search_on_string_space = ' '.join(search_on_list)
+    search_on_string_comma = ','.join(search_on_list)
 
     fruit_option_list = [pd_df.loc[pd_df['FRUIT_NAME'] == fruit, 'FRUIT_NAME'].iloc[0] for fruit in ingredients_list]
-    hash_using_fruit_option_space = calculate_hash(fruit_option_list, ' ')
-    hash_using_fruit_option_comma = calculate_hash(fruit_option_list, ',')
-    hash_using_fruit_option_comma_space = calculate_hash(fruit_option_list, ', ')
+    fruit_option_string_comma_space = ', '.join(fruit_option_list)
+    fruit_option_string_space = ' '.join(fruit_option_list)
+    fruit_option_string_comma = ','.join(fruit_option_list)
 
-    st.write(f"Hash using FRUIT_NAME with space as delimiter: {hash_using_fruit_name_space}")
-    st.write(f"Hash using FRUIT_NAME with comma as delimiter: {hash_using_fruit_name_comma}")
-    st.write(f"Hash using FRUIT_NAME with comma and space as delimiter: {hash_using_fruit_name_comma_space}")
-    st.write(f"Hash using SEARCH_ON with space as delimiter: {hash_using_search_on_space}")
-    st.write(f"Hash using SEARCH_ON with comma as delimiter: {hash_using_search_on_comma}")
-    st.write(f"Hash using SEARCH_ON with comma and space as delimiter: {hash_using_search_on_comma_space}")
-    st.write(f"Hash using FRUIT_OPTION with space as delimiter: {hash_using_fruit_option_space}")
-    st.write(f"Hash using FRUIT_OPTION with comma as delimiter: {hash_using_fruit_option_comma}")
-    st.write(f"Hash using FRUIT_OPTION with comma and space as delimiter: {hash_using_fruit_option_comma_space}")
+    # Calculate hashes using different concatenation techniques and sources
+    hash_fruit_name_space = calculate_hash(ingredients_list, ' ')
+    hash_fruit_name_comma = calculate_hash(ingredients_list, ',')
+    hash_fruit_name_comma_space = calculate_hash(ingredients_list, ', ')
+
+    hash_search_on_space = calculate_hash(search_on_list, ' ')
+    hash_search_on_comma = calculate_hash(search_on_list, ',')
+    hash_search_on_comma_space = calculate_hash(search_on_list, ', ')
+
+    hash_fruit_option_space = calculate_hash(fruit_option_list, ' ')
+    hash_fruit_option_comma = calculate_hash(fruit_option_list, ',')
+    hash_fruit_option_comma_space = calculate_hash(fruit_option_list, ', ')
+
+    st.write(f"Hash using FRUIT_NAME with space as delimiter: {hash_fruit_name_space}")
+    st.write(f"Hash using FRUIT_NAME with comma as delimiter: {hash_fruit_name_comma}")
+    st.write(f"Hash using FRUIT_NAME with comma and space as delimiter: {hash_fruit_name_comma_space}")
+    st.write(f"Hash using SEARCH_ON with space as delimiter: {hash_search_on_space}")
+    st.write(f"Hash using SEARCH_ON with comma as delimiter: {hash_search_on_comma}")
+    st.write(f"Hash using SEARCH_ON with comma and space as delimiter: {hash_search_on_comma_space}")
+    st.write(f"Hash using FRUIT_OPTION with space as delimiter: {hash_fruit_option_space}")
+    st.write(f"Hash using FRUIT_OPTION with comma as delimiter: {hash_fruit_option_comma}")
+    st.write(f"Hash using FRUIT_OPTION with comma and space as delimiter: {hash_fruit_option_comma_space}")
 
     # Default hash used in INSERT statement (update as necessary)
-    hash_ing = hash_using_fruit_name_comma_space
+    hash_ing = hash_fruit_name_comma_space
     st.write(f"Calculated hash: {hash_ing}")
 
     my_insert_stmt = f"""
     INSERT INTO smoothies.public.orders(ingredients, name_on_order, order_filled, hash_ing)
-    VALUES ('{ingredients_string}', '{name_on_order}', '{str(order_filled).upper()}', '{str(hash_ing)}')
+    VALUES ('{ingredients_string_comma_space}', '{name_on_order}', '{str(order_filled).upper()}', '{str(hash_ing)}')
     """
 
     st.write(my_insert_stmt)
