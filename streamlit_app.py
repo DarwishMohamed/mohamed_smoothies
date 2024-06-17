2b08e9f185f91525f622b9fd40b4439671d8e4c5
DarwishMohamed <109226826+DarwishMohamed@users.noreply.github.com>
2024-06-17 12:26:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,13 +16,12 @@ order_filled = st.checkbox('Mark order as filled')
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-
 # Convert the Snowpark dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients',
-    my_dataframe
+    pd_df['FRUIT_NAME'].tolist()
 )
 
 def calculate_hash(ingredients):
@@ -31,23 +30,20 @@ def calculate_hash(ingredients):
     return int(hashlib.md5(ingredients_string.encode()).hexdigest(), 16)
 
 if ingredients_list:
-    ingredients_string = ''
-
-    # Use search_on values for concatenation
-    search_on_list = []
+    ingredients_string_list = []
 
     for fruit_chosen in ingredients_list:
+        ingredients_string_list.append(fruit_chosen)
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        search_on_list.append(search_on)
         st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
 
         st.subheader(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
-    # Create the ingredients string from search_on values
-    ingredients_string = ' '.join(search_on_list).strip()
-    hash_ing = calculate_hash(search_on_list)
+    # Create the ingredients string from FRUIT_NAME values
+    ingredients_string = ' '.join(ingredients_string_list).strip()
+    hash_ing = calculate_hash(ingredients_string_list)
 
     my_insert_stmt = """
     INSERT INTO smoothies.public.orders(ingredients, name_on_order, order_filled, hash_ing)
